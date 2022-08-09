47c6081ae7e2b90582942a8bbd938f936b9199be
ishmaelewis40 <ishmaelewis40@gmail.com>
2022-08-09 12:21:27 +0100

    React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">amazonaa</a>
+      </header>
+      <main>Products list</main>
+    </div>
+  );
+}
+
+export default App;
